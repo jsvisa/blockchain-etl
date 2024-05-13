e9e253f6eab13d7f41a655f93d46c080ba076eea
jsvisa <delweng@gmail.com>
2024-05-13 03:53:03 +0000

    all: set devel version
    Signed-off-by: jsvisa <delweng@gmail.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = read("README.md") if os.path.isfile("README.md") else ""
 
 setup(
     name="blockchain-etl",
-    version="3.5.0",
+    version="devel",
     author="Delweng Zheng",
     author_email="delweng@gmail.com",
     description="Tools for exporting Ethereum/Bitcoin data into CSV/PostgreSQL",
