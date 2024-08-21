ad30e8d66e94304411b96b16baabf778b4b6e41f
jsvisa <delweng@gmail.com>
2024-08-21 09:39:23 +0800

    exporter/psycopg: don't reuse stream
    Signed-off-by: jsvisa <delweng@gmail.com>

--- blockchainetl/jobs/exporters/psycopg_item_exporter.py
+++ blockchainetl/jobs/exporters/psycopg_item_exporter.py
@@ -1,7 +1,7 @@
 import csv
 import io
 import psycopg2 as psycopg
-from typing import List, Dict
+from typing import Iterable, List, Dict
 from blockchainetl.streaming.postgres_utils import cursor_copy_from_stream
 from .converters.composite_item_converter import CompositeItemConverter
 from ._utils import group_by_item_type
@@ -10,7 +10,7 @@ from ._utils import group_by_item_type
 def list_of_dicts_to_csv(items: List[Dict]) -> str:
     # Use StringIO to simulate a file in memory
     output = io.StringIO()
-    items2stream(items, output)
+    fillin_items_to_stream(items, output)
 
     # Get the CSV string from the StringIO object
     csv_string = output.getvalue()
@@ -21,27 +21,34 @@ def list_of_dicts_to_csv(items: List[Dict]) -> str:
     return csv_string
 
 
-def items2stream(items: List[Dict], output: io.StringIO):
+def fillin_items_to_stream(items: Iterable[Dict], output: io.StringIO):
     # To reset the buffer position to the start so it can be read from the beginning
     output.seek(0)
     # Get the fieldnames from the keys of the first dictionary
-    if len(items) > 0:
-        fieldnames = items[0].keys()
-    else:
+    iterator = iter(items)
+    try:
+        fieldnames = next(iterator).keys()
+    except StopIteration:
         raise ValueError("The list cannot be empty")
 
     # Create a csv writer object
-    writer = csv.DictWriter(output, fieldnames=fieldnames)
+    writer = csv.DictWriter(output, fieldnames=fieldnames, delimiter="^")
 
     # Write the header
     writer.writeheader()
 
     # Write the data
-    for row in items:
+    for row in iterator:
         writer.writerow(row)
 
     # To reset the buffer position to the start so it can be read from the beginning
     output.seek(0)
+
+
+def items2stream(items: Iterable[Dict]) -> io.StringIO:
+    output = io.StringIO()
+    fillin_items_to_stream(items, output)
+    return output
 
 
 class PsycopgItemExporter:
@@ -66,7 +73,6 @@ class PsycopgItemExporter:
         items_grouped_by_type = group_by_item_type(items)
 
         rows = 0
-        stream = io.StringIO()
         with self.conn.cursor() as cursor:
             for item_type, table in self.item_type_to_table_mapping.items():
                 item_group = items_grouped_by_type.get(item_type)
@@ -74,12 +80,13 @@ class PsycopgItemExporter:
                     continue
 
                 tbl = "{}.{}".format(self.dbschema, table)
-                items2stream(list(self.convert_items(item_group)), stream)
+                # don't reuse the stream, else some wired data occupied issue will happen
+                stream = items2stream(self.convert_items(item_group))
 
                 rows += cursor_copy_from_stream(
-                    self.conn, cursor, tbl, stream, delimiter=","
+                    self.conn, cursor, tbl, stream, delimiter="^"
                 )
-        stream.close()
+                stream.close()
         return rows
 
     def convert_items(self, items):
